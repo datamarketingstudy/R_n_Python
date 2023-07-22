61da79f4e3d953ba12d43acfa61241b5bf9542aa
Geon-Seung LEE <dplyr@kakao.com>
2023-07-23 08:55:56 +0900

    [R code]데이터 불러오기

---
+++ 001_데이터불러오기/001_데이터불러오기.R
@@ -0,0 +1,23 @@
+# install.pakages("tidyverse") # 최초 설치 1회
+library(tidyverse)
+
+## File import
+
+### Customer 데이터 읽기
+customer <- read_csv("data/Customer.csv")
+head(customer) # 첫 6행 확인
+nrow(customer) # 데이터 총 행 수 확인
+str(customer) # 전체적인 데이터 구조 확인
+
+### Transactions 데이터 읽기
+transactions <- read_csv("data/transactions.csv")
+head(transactions) # 첫 6행 확인
+nrow(transactions) # 데이터 총 행 수 확인
+str(transactions) # 전체적인 데이터 구조 확인
+
+
+### prod_cat_info 데이터 읽기
+prod_cat_info <- read_csv("data/prod_cat_info.csv")
+head(prod_cat_info) # 첫 6행 확인
+nrow(prod_cat_info) # 데이터 총 행 수 확인
+str(prod_cat_info) # 전체적인 데이터 구조 확인
